a07ee5379e21b78d089a0599ae214c8ecf66d643
GaGiiiii <dragoslav.gagi8@gmail.com>
2020-10-30 13:37:07 +0100

    Added movie modals

--- includes/movie_modal.php
+++ includes/movie_modal.php
@@ -1,47 +1,53 @@
+<?php foreach ($movies as $movie) : ?>
+
+<?php $trailer = str_replace("watch?v=", "embed/", $movie['trailer']); ?>
+
 <!-- Modal -->
 <div class="movie-modal">
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <button type="button" class="close text-right" data-dismiss="modal" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      <div class="container mt-5">
+  <div class="modal fade" id="movie-modal<?php echo $movie['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <button type="button" class="close text-right" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+        <div class="container mt-5">
 
-        <div class="row">
-          <div class="col-md-12 bg-primary pb-5">
-            <h1 class="text-white pt-4 pb-2">AVENGERS: END GAME</h1>
-            <iframe width="100%" height="80%" src="https://www.youtube.com/embed/TcMBFSGVi1c"></iframe>
+          <div class="row">
+            <div class="col-md-12 bg-primary pb-5">
+              <h1 class="text-white pt-4 pb-2"><?php echo $movie['title'] ?? "Unknown"; ?></h1>
+              <iframe width="100%" height="80%" src="<?php echo $trailer ?? ""; ?>"></iframe>
+            </div>
           </div>
-        </div>
 
-        <div class="row mt-5 mb-5">
-          <div class="col-md-12 p-0">
-            <div class="movie-card py-3 px-1">
-              <div class="container-fluid">
-                <div class="row">
-                  <div class="col-md-3">
-                    <img src="https://wallpaperaccess.com/full/329583.jpg" alt="Banner1">
-                  </div>
-                  <div class="col-md-9">
-                    <p class="mb-0 text-left mb-0">Name: </p>
-                    <p class="mb-0 mt-2 text-left">Length: </p>
-                    <p class="text-left mt-2 mb-0">Genre: </p>
-                    <p class="text-left mt-2 mb-0">Price: </p>
-                    <p class="text-left mt-2 mb-0">Director: </p>
-                    <p class="text-left mt-2 mb-0">Actors: </p>
-                  </div>
-                  <div class="movie-description p-2 ml-3 border mt-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda quasi magni atque eligendi obcaecati libero? Incidunt blanditiis excepturi dolor vel doloribus autem fugiat, magnam consectetur culpa ut unde odit nihil provident eos repellendus assumenda. Tenetur nostrum hic aliquam quidem consequuntur maxime dicta? Modi tempora perferendis eveniet eos aperiam illum? Recusandae provident, autem numquam, voluptate dignissimos quae sed quos dolore dolores consequatur ipsam nihil velit, explicabo facilis ad sequi ut doloremque illo debitis obcaecati voluptatibus atque! Nam sapiente dolorum, provident asperiores, error magni perspiciatis, saepe et aliquam corporis a exercitationem delectus sit ducimus alias adipisci. Nisi aut eligendi beatae fuga debitis.
+          <div class="row mt-5 mb-5">
+            <div class="col-md-12 p-0">
+              <div class="movie-card py-3 px-1">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-md-3">
+                      <img src="<?php echo $movie['img'] ?? ""; ?>" alt="Banner1">
+                    </div>
+                    <div class="col-md-9">
+                      <p class="mb-0 text-left mb-0">Title: <?php echo $movie['title'] ?? ""; ?></p>
+                      <p class="mb-0 mt-2 text-left">Length: <?php echo $movie['length'] ?? ""; ?> mins</p>
+                      <p class="text-left mt-2 mb-0">Genre: <?php echo $movie['genre'] ?? ""; ?></p>
+                      <p class="text-left mt-2 mb-0">Price: <?php echo $movie['price'] ?? ""; ?> RSD</p>
+                      <p class="text-left mt-2 mb-0">Director: <?php echo $movie['director'] ?? ""; ?></p>
+                      <p class="text-left mt-2 mb-0">Actors: <?php echo $movie['actors'] ?? ""; ?></p>
+                    </div>
+                    <div class="movie-description p-2 ml-3 border mt-3">
+                      <?php echo $movie['description'] ?? ""; ?>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
+        </div>
       </div>
     </div>
   </div>
 </div>
-</div>
+
+<?php endforeach; ?>
--- includes/movies.php
+++ includes/movies.php
@@ -1,43 +1,42 @@
 <!-- SPECIAL PRICE -->
 <secion id="movies">
-    <div class="container mt-5 p-0">
-      <h1 class="ml-2">MOVIES</h1>
-      <hr>
-      <div class="button-group text-right">
-        <button class="btn sort-btn">Price</button>
-        <button class="btn sort-btn">Duration</button>
-        <form class="form-inline my-2 my-lg-0 d-inline-block">
-          <input class="form-control mr-sm-2 search-input" type="text" placeholder="Search">
-          <button class="btn btn-outline-secondary my-2 my-sm-0 search-btn" type="submit">Search</button>
-        </form>
-      </div>
+  <div class="container mt-5 p-0">
+    <h1 class="ml-2">MOVIES</h1>
+    <hr>
+    <div class="button-group text-right">
+      <button class="btn sort-btn">Price</button>
+      <button class="btn sort-btn">Duration</button>
+      <form class="form-inline my-2 my-lg-0 d-inline-block">
+        <input class="form-control mr-sm-2 search-input" type="text" placeholder="Search">
+        <button class="btn btn-outline-secondary my-2 my-sm-0 search-btn" type="submit">Search</button>
+      </form>
+    </div>
 
-      <div class="grid text-center">
+    <div class="grid text-center">
 
-        <div class="container-fluid">
-          <div class="row">
+      <div class="container-fluid">
+        <div class="row">
 
-            <?php foreach($movies as $movie) : ?>
+          <?php foreach ($movies as $movie) : ?>
 
             <div class="col-md-4">
               <div class="item text-center border p-3">
-                    <img src="<?php echo $movie['img'] ?? ""; ?>" alt="Banner1">
-                    <h3 class="text-center movie-title mt-3"><?php echo $movie['title'] ?? "Title not found"; ?></h3>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="fas fa-star"></i>
-                    <i class="far fa-star"></i>
-
-                </div>
+                <img src="<?php echo $movie['img'] ?? ""; ?>" alt="Banner1" data-toggle="modal" data-target="#movie-modal<?php echo $movie['id']; ?>">
+                <h3 class="text-center movie-title mt-3"><?php echo $movie['title'] ?? "Title not found"; ?></h3>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="far fa-star"></i>
+              </div>
             </div>
 
-            <?php endforeach; ?>
+          <?php endforeach; ?>
 
-          </div>
         </div>
-
       </div>
+
     </div>
-  </secion>
-  <!-- SPECIAL PRICE -->
+  </div>
+</secion>
+<!-- SPECIAL PRICE -->
