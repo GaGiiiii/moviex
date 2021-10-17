a4ad9e50d7cf6441ed89e7d45bc82a7ffeb32634
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-17 11:57:13 +0200

    Updates 2021

--- classes/Database.php
+++ classes/Database.php
@@ -1,34 +1,41 @@
-<?php 
+<?php
 
 
-  if (session_status() == PHP_SESSION_NONE) {
-    session_start();
-  }
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
 
-  class Database{
-    private $host = "localhost"; // Host
-    private $db_name = "moviex"; // DB Name
-    private $username = "root"; // DB Username
-    private $password = ""; // DB Password
+class Database {
+  private $host = "localhost"; // Host
+  private $db_name = "moviex"; // DB Name
+  private $username = "root"; // DB Username
+  private $password = ""; // DB Password
 
-    private static $instance = null; // Instanca klase
-    public $connection = null; // Konekcija
+  private static $instance = null; // Instanca klase
+  public $connection = null; // Konekcija
 
-    private function __construct(){    
-      $this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';charset=utf8mb4', $this->username, $this->password);
-      $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      // $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
+  private function __construct() {
+    if ($_SERVER['SERVER_NAME'] != "localhost") {
+      $this->host = $_ENV['dbhost']; // Host
+      $this->db_name = $_ENV['dbname']; // DB Name
+      $this->username = $_ENV['dbusername']; // DB Username
+      $this->password = $_ENV['dbpassword']; // DB Password    
     }
 
-    public function getConnection(){
-        return $this->connection;
-    }
-    
-    public static function getInstance(){
-        if(!isset(self::$instance)){
-            self::$instance = new Database();
-        }
-
-        return self::$instance;
-    }   
+    $this->connection = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name . ';charset=utf8mb4', $this->username, $this->password);
+    $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->db_name);
   }
+
+  public function getConnection() {
+    return $this->connection;
+  }
+
+  public static function getInstance() {
+    if (!isset(self::$instance)) {
+      self::$instance = new Database();
+    }
+
+    return self::$instance;
+  }
+}
--- includes/globals.php
+++ includes/globals.php
@@ -1,3 +1,7 @@
-<?php 
+<?php
 
-  define("SITE_URL", "http://localhost/itehdomaci/");
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+  define("SITE_URL", "https://localhost/itehdomaci/");
+} else {
+  define("SITE_URL", "https://moviexg.herokuapp.com/");
+}
